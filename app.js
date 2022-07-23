fec97ef7f7b4b4bfa55cf0802997c013079629b2
RussiSunni <jonathandyason@hotmail.com>
2022-07-23 14:44:37 +0200

    update

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require('express')
 const app = express()
 
+app.use(express.static("public"))
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-    res.render("index", {text: "world"})
+    res.render("index")
 })
+
+
+app.get('/pathways/yellow', (req, res) => {
+    res.render("games/pathways")
+})
+
+app.get('/pathways/blue', (req, res) => {
+    res.render("games/pathways")
+})
+
 
 const userRouter = require('./routes/users')
 
