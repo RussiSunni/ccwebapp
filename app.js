2c04ddc773baf31b054f573afd0db5179bf63553
RussiSunni <jonathandyason@hotmail.com>
2022-07-28 12:10:14 +0200

    Adding db and users

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express')
+const bodyParser = require('body-parser');
 const app = express()
 
 app.use(express.static("public"))
 app.set('view engine', 'ejs')
+
+// parse application/json
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.render("index")
@@ -21,6 +26,7 @@ app.get('/pathways/blue', (req, res) => {
 const userRouter = require('./routes/users')
 
 app.use('/users', userRouter)
+
 
 
 app.listen(3000)
--- routes/users.js
+++ routes/users.js
@@ -1,20 +1,93 @@
 const express = require('express')
 const router = express.Router()
 
+
+const mysql = require('mysql');
+
+/*------------------------------------------
+--------------------------------------------
+Database Connection
+--------------------------------------------
+--------------------------------------------*/
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'conscious_coding'
+});
+
+/*------------------------------------------
+--------------------------------------------
+Shows Mysql Connect
+--------------------------------------------
+--------------------------------------------*/
+conn.connect((err) => {
+    if (err) throw err;
+    console.log('Mysql connected...');
+});
+
+
+/**
+ * Get All Items
+ *
+ * @return response()
+ */
 router.get('/', (req, res) => {
-    res.send("user list")
+    let sqlQuery = "SELECT * FROM users";
+    let query = conn.query(sqlQuery, (err, results) => {
+        if (err) throw err;
+        res.render('list-users', { students: results });
+    });
+});
+
+
+
+// /**
+//  * Get Single Item
+//  *
+//  * @return response()
+//  */
+// router.get('/:id', (req, res) => {
+//     //let sqlQuery = "SELECT * FROM users WHERE id=" + req.params.id;
+
+//     let sqlQuery = `SELECT first_name, last_name, name AS skill_name, is_mastered, parent
+//     FROM skill_tree.user_skills 
+//     JOIN skill_tree.users 
+//     ON user_skills.user_id = users.id 
+//     JOIN skill_tree.skills 
+//     ON user_skills.skill_id = skills.id 
+//     WHERE user_id =` + req.params.id;
+
+//     let query = conn.query(sqlQuery, (err, results) => {
+//         if (err) throw err;
+//         res.render('show-user', { student: results });
+//     });
+// });
+
+
+
+
+// Add user
+router.get('/add', (req, res) => {
+    res.render('add-user')
 })
 
-router.get('/new', (req, res) => {
-    res.send("add new user")
-})
-
+/**
+ * Create New Item
+ *
+ * @return response()
+ */
 router.post('/', (req, res) => {
-    res.send("create user")
-})
+    let data = { first_name: req.body.first_name, last_name: req.body.last_name };
+    let sqlQuery = "INSERT INTO users SET ?";
+    let query = conn.query(sqlQuery, data, (err, results) => {
+        if (err) throw err;
+        res.send(apiResponse(results));
+    });
+});
 
-router.get('/:id', (req, res) => {    
-    res.send('get user with ID ${req.params.id}')
-})
+
+
+
 
 module.exports = router
