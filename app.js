b3e940703866e41033b238c646ee2166c5415411
RussiSunni <jonathandyason@hotmail.com>
2022-08-25 10:15:43 +0200

    setting up games

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
 const express = require('express')
-const bodyParser = require('body-parser');
 const app = express()
 
-app.use(express.static("public"))
-app.set('view engine', 'ejs')
-
-// parse application/json
+// Middleware ---------
+// Need this below middleware to POST
+const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(express.static("public"))
+app.set('view engine', 'ejs')
 
 // Home or Create Game page.
 app.get('/', (req, res) => {
     res.render("index")
 })
-
 
 // The Pathways WebGL export page.
 app.get('/games/pathways', (req, res) => {
@@ -23,45 +22,13 @@ app.get('/games/pathways', (req, res) => {
 
 // -----------------------------------------
 
-const mysql = require('mysql');
-
-/*------------------------------------------
---------------------------------------------
-Database Connection
---------------------------------------------
---------------------------------------------*/
-// const conn = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     //password: 'C0nsc!0u5C0d!ng2022',
-//     password: '',
-//     database: 'conscious_coding'
-// });
-
-// /*------------------------------------------
-// --------------------------------------------
-// Shows Mysql Connect
-// --------------------------------------------
-// --------------------------------------------*/
-// conn.connect((err) => {
-//     if (err) {
-//         console.log("MySQL not connected problem");
-//         // throw err;
-//     }
-//     else {
-//         console.log('Mysql connected...');
-//     }
-// });
-
-
-// ------------------------------------------
-
-
-
 const userRouter = require('./routes/users')
 app.use('/users', userRouter)
 
 const cohortRouter = require('./routes/cohorts')
 app.use('/cohorts', cohortRouter)
+
+const gamesRouter = require('./routes/games')
+app.use('/games', gamesRouter)
 
 app.listen(3000)
--- routes/games.js
+++ routes/games.js
@@ -1,87 +1,75 @@
-// const express = require('express')
-// const router = express.Router()
+const express = require('express')
+const router = express.Router()
+const mysql = require('mysql');
+
+/*------------------------------------------
+--------------------------------------------
+Database Connection
+--------------------------------------------
+--------------------------------------------*/
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    //password: 'C0nsc!0u5C0d!ng2022',
+    database: 'conscious_coding'
+});
+
+/*------------------------------------------
+--------------------------------------------
+Shows Mysql Connect
+--------------------------------------------
+--------------------------------------------*/
+conn.connect((err) => {
+    if (err) {
+        console.log("MySQL not connected problem");
+        // throw err;
+    }
+    console.log('Mysql connected...');
+});
+
+/**
+ * Create New Item
+ *
+ * @return response()
+ */
+router.post('/add', (req, res) => {
+    let data = { name: req.body.name, number_rounds: req.body.rounds, number_moves: req.body.turns, number_seconds: req.body.seconds, points_toggle: req.body.toggle_points, points_endpoint: req.body.endpoint_points };
+    let sqlQuery = "INSERT INTO games SET ?";
+    let query = conn.query(sqlQuery, data, (err, results) => {
+        if (err) throw err;
+        res.render('index');
+    });
+});
 
 
-// const mysql = require('mysql');
+/**
+ * Get All Games
+ *
+ * @return response() 
+ */
+router.get('/list', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    let sqlQuery = "SELECT * FROM games";
+    let query = conn.query(sqlQuery, (err, results) => {
+        if (err) throw err;
+        res.json(results);
+    });
+});
 
-// /*------------------------------------------
-// --------------------------------------------
-// Database Connection
-// --------------------------------------------
-// --------------------------------------------*/
-// const conn = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: '',
-//     //password: 'C0nsc!0u5C0d!ng2022',
-//     database: 'conscious_coding'
-// });
-
-// /*------------------------------------------
-// --------------------------------------------
-// Shows Mysql Connect
-// --------------------------------------------
-// --------------------------------------------*/
-// conn.connect((err) => {
-//     if (err) {
-//         console.log("MySQL not connected problem");
-//         // throw err;
-//     }
-//     console.log('Mysql connected...');
-// });
-
-
-// /**
-//  * Get All Sessions
-//  *
-//  * @return response()
-//  */
-// router.get('/', (req, res) => {
-//     let sqlQuery = "SELECT * FROM games";
-//     let query = conn.query(sqlQuery, (err, results) => {
-//         if (err) throw err;
-//         res.render('list-games', { games: results });
-//     });
-// });
+/**
+ * Get One Game
+ *
+ * @return response() 
+ */
+router.get('/:id', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    let sqlQuery = "SELECT * FROM games WHERE id=" + req.params.id;
+    let query = conn.query(sqlQuery, (err, results) => {
+        if (err) throw err;
+        res.json(results);
+    });
+});
 
 
-
-
-
-// // Add session
-// router.get('/add', (req, res) => {
-//     res.render('add-game')
-// })
-
-// /**
-//  * Create New Session
-//  *
-//  * @return response()
-//  */
-// router.post('/', (req, res) => {
-//     let data = { name: req.body.name };
-//     let sqlQuery = "INSERT INTO games SET ?";
-//     let query = conn.query(sqlQuery, data, (err, results) => {
-//         if (err) throw err;
-//         res.send(apiResponse(results));
-//     });
-// });
-
-
-// /**
-//  * Create New Item
-//  *
-//  * @return response()
-//  */
-//  router.post('/', (req, res) => {
-//     let data = { name: req.body.name, number_rounds: req.body.rounds, number_moves: req.body.turns, number_seconds: req.body.seconds, points_toggle: req.body.toggle_points, points_endpoint: req.body.endpoint_points };
-//     let sqlQuery = "INSERT INTO cohorts SET ?";
-//     let query = conn.query(sqlQuery, data, (err, results) => {
-//         if (err) throw err;
-//         res.send("cohort added");
-//         res.end();
-//     });
-// });
-
-
-// module.exports = router
+module.exports = router
