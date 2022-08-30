6aae2e75b4cbc2610d0769947b3f99da7799b280
RussiSunni <jonathandyason@hotmail.com>
2022-08-30 12:33:47 +0200

    fgh

--- routes/games.js
+++ routes/games.js
@@ -10,8 +10,8 @@ Database Connection
 const conn = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '',
-    //password: 'C0nsc!0u5C0d!ng2022',
+    //password: '',
+    password: 'C0nsc!0u5C0d!ng2022',
     database: 'conscious_coding'
 });
 
